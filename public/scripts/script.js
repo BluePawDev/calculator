1010423e9df90917eda7714d6995399a882d1e01
BluePawDev <jaruby@me.com>
2017-05-28 13:42:17 -0500

    Commit Base mode complete

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,25 +2,109 @@ console.log('js/jq sourced');
 
 $(onReady);
 
-function onReady(){
-  $('#cmdDivide').on('click', divide());
-  $('#cmdMultiply').on('click', multiply());
-  $('#cmdSubtract').on('click', subtract());
-  $('#cmdAdd').on('click', add());
-}
+// start of onReady
+function onReady() {
+  // set txtResult to disabled
+  $('#txtResult').attr('disabled', 'disabled');
 
-function divide(numOne, numTwo) {
-  console.log('cmdDivide clicked');
-}
+  // start of cmdClear on-click
+  $('#cmdClear').on('click', function() {
+    console.log('Clear clicked');
+    $('#txtInputOne').val('');
+    $('#txtInputTwo').val('');
+    $('#txtResult').val(' ');
+  }); // end of cmdClear on-click
 
-function multiply(numOne, numTwo){
-  console.log('cmdMultiply clicked');
-}
+  // start of cmdDivide on-click
+  $('#cmdDivide').on('click', function() {
+    console.log('Divide clicked');
+    // define obj to POST on divide
+    var objToDivide = {
+      numOne: $('#txtInputOne').val(),
+      numTwo: $('#txtInputTwo').val(),
+      actType: 'divide'
+    }; // end of obj to divide
+    console.log(objToDivide);
+    // Ajax POST toDivide definition
+    $.ajax({
+      type: 'POST',
+      url: '/toDivide',
+      data: objToDivide,
+      // define success
+      success: function(response) {
+        console.log(response);
+        $('#txtResult').val(response.resultDiv);
+      } // end of success definition
+    }); // end of Ajax POST toDivide
+  }); // end of cmdDivide on-click
 
-function subtract(numOne, numTwo){
-  console.log('cmdSubtract clicked');
-}
+// start cmdMultiply on-click
+  $('#cmdMultiply').on('click', function() {
+    console.log('Multiply clicked');
+    // define obj to POST on Multiply
+    var objToMultiply = {
+      numOne: $('#txtInputOne').val(),
+      numTwo: $('#txtInputTwo').val(),
+      actType: 'multiply'
+    }; // end of obj to multiply
+    console.log(objToMultiply);
+    // Ajax POST toMultiply definition
+    $.ajax({
+      type: 'POST',
+      url: '/toMultiply',
+      data: objToMultiply,
+      // define success
+      success: function(response) {
+        console.log(response);
+        $('#txtResult').val(response.resultMultiply);
+      } // end of success definition
+    }); // end of Ajax POST toMultiply
+  });
 
-function add(numOne, numTwo){
-  console.log('cmdAdd clicked');
-}
+  // start of cmdSubtract on-click
+  $('#cmdSubtract').on('click', function() {
+    console.log('Subtract clicked');
+    // define obj to POST on subtract
+    var objToSubtract = {
+      numOne: $('#txtInputOne').val(),
+      numTwo: $('#txtInputTwo').val(),
+      actType: 'subtract'
+    }; // end of obj to subtract definition
+    console.log(objToSubtract);
+    // Ajax POST toSubtract definition
+    $.ajax({
+      type: 'POST',
+      url: '/toSubtract',
+      data: objToSubtract,
+      // define success
+      success: function(response) {
+        console.log(response);
+        $('#txtResult').val(response.resultSubtract);
+      } // end of success definition
+    }); // end of Ajax POST toSubtract
+  }); // end of cmdSubtract on-click
+
+  // start of cmdAdd on-click
+  $('#cmdAdd').on('click', function() {
+    console.log('Add clicked');
+    // define obj to POST on add
+    var objToAdd = {
+      numOne: $('#txtInputOne').val(),
+      numTwo: $('#txtInputTwo').val(),
+      actType: 'add'
+    }; // end of obj to add definition
+    console.log(objToAdd);
+    // Ajax POST toAdd definition
+    $.ajax({
+      type: 'POST',
+      url: '/toAdd',
+      data: objToAdd,
+      // define success
+      success: function(response) {
+        console.log(response);
+        $('#txtResult').val(response.resultAdd);
+      } // end of success definition
+    }); // end of Ajax POST toAdd
+  }); // end of cmdAdd on-click
+
+} // end of onReady
--- server.js
+++ server.js
@@ -17,3 +17,39 @@ app.get('/', function(req, res){
   console.log('base URL was hit');
   res.sendFile(path.resolve('views/index.html'));
 });
+
+app.post('/toDivide', function(req, res){
+  numOne = Number(req.body.numOne);
+  numTwo = Number(req.body.numTwo);
+  var responseDivide = {
+    resultDiv: numOne / numTwo
+  };
+  res.send(responseDivide);
+});
+
+app.post('/toMultiply', function(req, res){
+  numOne = Number(req.body.numOne);
+  numTwo = Number(req.body.numTwo);
+  var responseMultiply = {
+    resultMultiply: numOne * numTwo
+  };
+  res.send(responseMultiply);
+});
+
+app.post('/toSubtract', function(req, res){
+  numOne = Number(req.body.numOne);
+  numTwo = Number(req.body.numTwo);
+  var responseSubtract = {
+    resultSubtract: numOne - numTwo
+  };
+  res.send(responseSubtract);
+});
+
+app.post('/toAdd', function(req, res){
+  numOne = Number(req.body.numOne);
+  numTwo = Number(req.body.numTwo);
+  var responseAdd = {
+    resultAdd: numOne + numTwo
+  };
+  res.send(responseAdd);
+});
